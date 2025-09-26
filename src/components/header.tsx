43e98b59161558b2dce9dbeb66de589a17a582e1
Leonardo Sabbadin <leosabbadinads01@gmail.com>
2025-09-26 19:23:16 +0000

    no mobile, ao selecionar uma opção como estudo, bonus ou corrigir redaçã

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { BookOpen, BotMessageSquare, Menu, PenSquare, Sparkles } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from '@/components/ui/sheet';
 
 export default function Header() {
   return (
@@ -48,24 +48,30 @@ export default function Header() {
               </SheetTrigger>
               <SheetContent side="right" className="w-[250px] sm:w-[300px]">
                 <div className="flex flex-col space-y-4 pt-8">
-                  <Button variant="ghost" asChild className="justify-start text-lg">
-                    <Link href="/estudo">
-                      <BookOpen className="mr-3 h-5 w-5" />
-                      Estudo
-                    </Link>
-                  </Button>
-                  <Button variant="ghost" asChild className="justify-start text-lg">
-                    <Link href="/bonus">
-                      <Sparkles className="mr-3 h-5 w-5" />
-                      Bônus
-                    </Link>
-                  </Button>
-                   <Button asChild className="font-bold uppercase tracking-wide text-lg">
-                    <Link href="/">
-                      <BotMessageSquare className="mr-3 h-5 w-5" />
-                      Corrigir Redação
-                    </Link>
-                  </Button>
+                  <SheetClose asChild>
+                    <Button variant="ghost" asChild className="justify-start text-lg">
+                      <Link href="/estudo">
+                        <BookOpen className="mr-3 h-5 w-5" />
+                        Estudo
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button variant="ghost" asChild className="justify-start text-lg">
+                      <Link href="/bonus">
+                        <Sparkles className="mr-3 h-5 w-5" />
+                        Bônus
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                   <SheetClose asChild>
+                    <Button asChild className="font-bold text-lg justify-start">
+                      <Link href="/">
+                        <BotMessageSquare className="mr-3 h-5 w-5" />
+                        Corrigir Redação
+                      </Link>
+                    </Button>
+                  </SheetClose>
                 </div>
               </SheetContent>
             </Sheet>
