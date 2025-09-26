9eaca09aeffde3a4e490fc281db4d063983cd3b7
Leonardo Sabbadin <leosabbadinads01@gmail.com>
2025-09-26 21:29:24 +0000

    mudou a mensagem mas continua

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,9 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   "projectId": "studio-7445211165-728a7",
   "appId": "1:358291345411:web:4c43d8cf0ad5e4f206fe36",
+  "storageBucket": "studio-7445211165-728a7.appspot.com",
   "apiKey": "AIzaSyD2F59n6fq44ljvsszHnlZj2Ej0iDUOo3Y",
   "authDomain": "studio-7445211165-728a7.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "358291345411",
   "databaseURL": "https://studio-7445211165-728a7-default-rtdb.firebaseio.com"
 };
