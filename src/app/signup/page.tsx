87199c7ae624597a3e1ffc719ab148ecdcf7458f
leosabbadin <leosabbadinads01@gmail.com>
2025-10-02 17:16:18 -0300

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -19,7 +19,7 @@ export default function SignupPage() {
         <CardContent className="flex flex-col items-center gap-6">
           <Button asChild className="w-full font-bold py-8 text-lg">
             <a
-              href="https://wa.me/5562996564956?text=Ol%C3%A1!%20Adquiri%20o%20m%C3%A9todo%20Hackeando%20a%20Reda%C3%A7%C3%A3o%20do%20Enem%20e%20quero%20meu%20Login%20e%20Senha.%20Meu%20e-mail%20%C3%A9%3A"
+              href="https://wa.me/5562996105483?text=Ol%C3%A1!%20Adquiri%20o%20m%C3%A9todo%20Hackeando%20a%20Reda%C3%A7%C3%A3o%20do%20Enem%20e%20quero%20meu%20Login%20e%20Senha.%20Meu%20e-mail%20%C3%A9%3A"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -28,7 +28,7 @@ export default function SignupPage() {
             </a>
           </Button>
           <p className="text-sm text-muted-foreground font-semibold">
-            (62) 99656-4956 e nos envie seu E-mail!
+            (62) 99610-5483 e nos envie seu E-mail!
           </p>
         </CardContent>
         <CardFooter className="flex-col text-center text-sm gap-4">
